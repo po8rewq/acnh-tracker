a0cade3eea83b1ee4d3b2a7728ff96f6884ec4ce
Adrien Fischer <adrien@feedr.co>
2020-05-10 11:31:48 +0100

    code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Container } from 'reactstrap';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Header from './components/Header';
-import FishTable from './components/FishTable';
-import BugsTable from './components/BugsTable';
-import FossilsTable from './components/FossilsTable';
 import useLocalStorage from './hooks/useLocalStorage';
-import RecipeList from './components/RecipeList';
-import TurnipsPage from './components/TurnipsPage';
+import Loading from './components/Loading';
+
+const RecipeList = lazy(() => import('./components/RecipeList'));
+const TurnipsPage = lazy(() => import('./components/TurnipsPage'));
+const FishTable = lazy(() => import('./components/FishTable'));
+const BugsTable = lazy(() => import('./components/BugsTable'));
+const FossilsTable = lazy(() => import('./components/FossilsTable'));
 
 const App = () => {
   const [hemisphere, setHemisphere] = useLocalStorage('hemisphere', 'northern');
@@ -15,25 +18,27 @@ const App = () => {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Header />
-      <Container fluid="md" style={{ marginTop: '15px' }}>
-        <Switch>
-          <Route path="/diy/:event">
-            <RecipeList />
-          </Route>
-          <Route path="/bugs">
-            <BugsTable hemisphere={hemisphere} setHemisphere={setHemisphere} />
-          </Route>
-          <Route path="/fossils">
-            <FossilsTable />
-          </Route>
-          <Route path="/turnips">
-            <TurnipsPage />
-          </Route>
-          <Route path="/">
-            <FishTable hemisphere={hemisphere} setHemisphere={setHemisphere} />
-          </Route>
-        </Switch>
-      </Container>
+      <Suspense fallback={<Loading />}>
+        <Container fluid="md" style={{ marginTop: '15px' }}>
+          <Switch>
+            <Route path="/diy/:event">
+              <RecipeList />
+            </Route>
+            <Route path="/bugs">
+              <BugsTable hemisphere={hemisphere} setHemisphere={setHemisphere} />
+            </Route>
+            <Route path="/fossils">
+              <FossilsTable />
+            </Route>
+            <Route path="/turnips">
+              <TurnipsPage />
+            </Route>
+            <Route path="/">
+              <FishTable hemisphere={hemisphere} setHemisphere={setHemisphere} />
+            </Route>
+          </Switch>
+        </Container>
+      </Suspense>
     </Router>
   );
 };
---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+const Loading = () => (
+  <Container style={{ textAlign: 'center' }}>Loading...</Container>
+);
+
+export default Loading;
